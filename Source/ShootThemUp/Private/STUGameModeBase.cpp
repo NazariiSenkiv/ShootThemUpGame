93663caf40275aab6bc884a6ae97e33416e311d4
NazariiSenkiv <95177352+NazariiSenkiv@users.noreply.github.com>
2022-07-13 19:23:20 +0300

    Added character class

--- Source/ShootThemUp/Private/STUGameModeBase.cpp
+++ Source/ShootThemUp/Private/STUGameModeBase.cpp
@@ -2,4 +2,11 @@
 
 
 #include "STUGameModeBase.h"
+#include "STUBaseCharacter.h"
+#include "STUPlayerController.h"
 
+ASTUGameModeBase::ASTUGameModeBase()
+{
+    DefaultPawnClass = ASTUBaseCharacter::StaticClass();
+    PlayerControllerClass = ASTUPlayerController::StaticClass();
+}
